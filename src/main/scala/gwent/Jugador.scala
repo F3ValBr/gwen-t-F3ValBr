b5ae0767644773b0b5cc825659d81e49aa364ee1
F3ValBr <89220737+F3ValBr@users.noreply.github.com>
2023-04-01 11:44:47 -0300

    Add: Tests sobre el nombre de jugador

--- src/main/scala/gwent/Jugador.scala
+++ src/main/scala/gwent/Jugador.scala
@@ -2,5 +2,16 @@ package cl.uchile.dcc
 package gwent
 
 class Jugador(val nombre: String) {
-
+  
+  
+  
+  
+  override def equals(other: Any): Boolean = {
+    if (other.isInstanceOf[Jugador]) {
+      val foo = other.asInstanceOf[Jugador]
+      nombre == foo.nombre
+    } else {
+      false
+    }
+  }
 }
--- src/test/scala/JugadorTest.scala
+++ src/test/scala/JugadorTest.scala
@@ -10,13 +10,18 @@ class JugadorTest extends munit.FunSuite {
   override def beforeEach(context: BeforeEach): Unit = {
     Jugador1 = Jugador("Jugador 1")
     Jugador2 = Jugador("Jugador 2")
-    Jugador3 = Jugador()
+    //Jugador3 = Jugador()
   }
 
   test("Un jugador debe tener nombre"){
-    assertEquals(Jugador1.nombre, expected = "Jugador 1")
-    assertEquals(Jugador2.nombre, expected = "Jugador 2")
-    assertEquals(Jugador3.nombre)
+    assertEquals(Jugador1.nombre, expected = "Jugador 1") // Jugador 1 corresponde a su nombre
+    assertEquals(Jugador2.nombre, expected = "Jugador 2") // Jugador 2 corresponde
+    assert(!Jugador1.nombre.equals(Jugador2.nombre))      // Jugadores con distintos nombres son distintos
+    assertEquals(new Jugador("Jugador4"),new Jugador("Jugador4")) // Crear un nuevo jugador sigue igual a crear el mismo
   }
 
+  test("El jugador tiene una seccion en el tablero"){
+
+
+  }
 }
