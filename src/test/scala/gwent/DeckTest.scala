1b69a1f50c585c5d4068037d9960d0f57210d5c1
F3ValBr <89220737+F3ValBr@users.noreply.github.com>
2023-04-19 13:59:36 -0400

    ADD: Test adicionales para DeckTest

--- src/test/scala/gwent/DeckTest.scala
+++ src/test/scala/gwent/DeckTest.scala
@@ -3,6 +3,7 @@ package gwent
 
 import gwent.cartas.{Carta, DeckClass}
 
+import gwent.cartas.cartaunidad.CartaAsedio
 import munit.FunSuite
 
 import scala.collection.mutable.ListBuffer
@@ -13,13 +14,20 @@ import scala.collection.mutable.ListBuffer
   */
 class DeckTest extends FunSuite{
   var deck1: DeckClass = _
+  var deck2: DeckClass = _
 
   override def beforeEach(context: BeforeEach): Unit = {
     deck1 = new DeckClass()
+    deck2 = new DeckClass(List(CartaAsedio("X",1)))
   }
 
-  test("Deck tiene 25 cartas"){
+  test("Deck tiene 25 cartas, y no puede tener menos"){
+    // deck con 25 cartas
     assertEquals(deck1.cant_cards(), 25)
+    assert(deck1.isValidDeck())
+    // deck con 1 carta
+    assertEquals(deck2.cant_cards(), 1)
+    assert(!deck2.isValidDeck())
   }
   test("Se le puede quitar una cantidad de cartas a deck"){
     for (i <- 1 to 5) deck1.draw_card()
@@ -40,6 +48,14 @@ class DeckTest extends FunSuite{
   }
   test("Se puede obtener una carta de un deck"){
     val carta: Carta = deck1.draw_card()
+    assertEquals(deck1.cant_cards(), 24)
+    assertEquals(deck1._deck.contains(carta), false)
+  }
+  test("Se puede remover una carta del deck"){
+    val carta: Carta = new CartaAsedio("1",1)
+    assertEquals(deck1.cant_cards(), 25)
+    assertEquals(deck1._deck.contains(carta), true)
+    deck1.remove_card(carta)
     assertEquals(deck1.cant_cards(), 24)
     assertEquals(deck1._deck.contains(carta), false)
   }
