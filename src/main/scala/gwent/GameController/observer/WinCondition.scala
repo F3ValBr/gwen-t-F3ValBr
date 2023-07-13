6c0dc150f6ae9101f62c5d1d1d0347962a6c0f3c
F3ValBr <89220737+F3ValBr@users.noreply.github.com>
2023-07-13 01:06:59 -0400

    INIT: Entrega Final - Tarea 3

--- src/main/scala/gwent/GameController/observer/WinCondition.scala
+++ src/main/scala/gwent/GameController/observer/WinCondition.scala
@@ -1,4 +1,12 @@
 package cl.uchile.dcc
 package gwent.GameController.observer
 
+/**
+ * Clase que representa una condición de victoria.
+ * @param name Nombre de la condición de victoria.
+ *
+ * @constructor Crea una condición de victoria con el nombre dado.
+ *
+ * @version 1.0
+ */
 class WinCondition(val name: String)
